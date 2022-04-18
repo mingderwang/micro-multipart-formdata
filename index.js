58dfd1455eff49f4db89f7722612b14421528e58
Ming-der Wang <mingderwang@gmail.com>
2022-04-18 14:44:20 +0800

    status code 403

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const { buffer, text, json } = require("micro");
+const { send } = require("micro");
 
 module.exports = async (req, res) => {
-  //const buf = await buffer(req, { limit: "1mb", encoding: "utf8" });
-  const buf = await buffer(req);
-  console.log(buf);
-  // <Buffer 7b 22 70 72 69 63 65 22 3a 20 39 2e 39 39 7d>
-  const txt = await text(req);
-  console.log(txt);
-  // '{"price": 9.99}'
-  const js = await json(req);
-  console.log(js.price);
-  // 9.99
-  return "";
+  const statusCode = 403;
+  const data = { error: "Custom error message" };
+
+  send(res, statusCode, data);
 };
-
-/* 
-curl --location --request POST 'localhost:3000' \
---header 'Content-Type: text/plain' \
---data-raw '{"price": 9.99}'
-*/
-
-// output
-
-/*
-<Buffer 7b 22 70 72 69 63 65 22 3a 20 39 2e 39 39 7d>
-{"price": 9.99}
-9.99
-< #3 200 [+4ms]
-*/
