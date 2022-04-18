fac3aeb4f0f51050e148464dd8416f9ef3b00ab9
Ming-der Wang <mingderwang@gmail.com>
2022-04-18 14:46:28 +0800

    add status code usage

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ module.exports = async (req, res) => {
 
   send(res, statusCode, data);
 };
+
+/*
+Use require('micro').send.
+statusCode is a Number with the HTTP status code, and must always be supplied.
+If data is supplied it is sent in the response. Different input types are processed appropriately, and Content-Type and Content-Length are automatically set.
+Stream: data is piped as an octet-stream. Note: it is your responsibility to handle the error event in this case (usually, simply logging the error and aborting the response is enough).
+Buffer: data is written as an octet-stream.
+object: data is serialized as JSON.
+string: data is written as-is.
+If JSON serialization fails (for example, if a cyclical reference is found), a 400 error is thrown. See Error Handling.
+*/
