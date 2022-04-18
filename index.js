7d07c868cadb689c6283a1d138f6f79f2a03c287
Ming-der Wang <mingderwang@gmail.com>
2022-04-18 22:46:26 +0800

    urlencoded-body-parsing

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const { buffer, text, json } = require("micro");
+const parse = require("urlencoded-body-parser");
 
-module.exports = async (req, res) => {
-  //const buf = await buffer(req, { limit: "1mb", encoding: "utf8" });
-  const buf = await buffer(req);
-  console.log(buf);
-  // <Buffer 7b 22 70 72 69 63 65 22 3a 20 39 2e 39 39 7d>
-  const txt = await text(req);
-  console.log(txt);
-  // '{"price": 9.99}'
-  const js = await json(req);
-  console.log(js.price);
-  // 9.99
-  return "";
+module.exports = async (req) => {
+  const data = await parse(req, ({ limit = "1mb" } = {}));
+  console.log(data);
+
+  return "Data logged to your console";
 };
-
-/* 
-curl --location --request POST 'localhost:3000' \
---header 'Content-Type: text/plain' \
---data-raw '{"price": 9.99}'
-*/
-
-// output
-
-/*
-<Buffer 7b 22 70 72 69 63 65 22 3a 20 39 2e 39 39 7d>
-{"price": 9.99}
-9.99
-< #3 200 [+4ms]
-*/
