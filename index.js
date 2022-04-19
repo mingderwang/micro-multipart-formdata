9245afe7ef3151418f3e9d8bc914d37f9410cc25
Ming-der Wang <mingderwang@gmail.com>
2022-04-19 13:32:38 +0800

    test json data

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { send } = require("micro");
 const { router, get } = require("microrouter");
+const data = require("./data.json");
 
 const hello = async (req, res) =>
   send(res, 200, await Promise.resolve(`Hello ${req.params.who}`));
 const notfound = (req, res) => send(res, 404, "Not found route");
-
-module.exports = router(get("/hello/:who", hello), get("/*", notfound));
+const test = (req, res) => {
+  send(res, 200, data);
+};
+module.exports = router(
+  get("/hello/:who", hello),
+  get("/data", test),
+  get("/*", notfound)
+);
