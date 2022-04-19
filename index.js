b306b548b3b0ce740e3ba944f1b19d1138f64fbd
Ming-der Wang <mingderwang@gmail.com>
2022-04-19 10:57:08 +0800

    add microrouter and micro-dev to main

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = (req, res) => {
-  res.end("Welcome to Micro");
-};
+const { send } = require("micro");
+const { router, get } = require("microrouter");
+
+const hello = async (req, res) =>
+  send(res, 200, await Promise.resolve(`Hello ${req.params.who}`));
+const notfound = (req, res) => send(res, 404, "Not found route");
+
+module.exports = router(get("/hello/:who", hello), get("/*", notfound));
